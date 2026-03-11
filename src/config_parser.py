60dd8cf2fe32ae8e67582375eded57ee8fc9f9ca
makl812c <makl812c@tu-dresden.de>
2026-03-11 16:45:48 +0100

    fixed tests

--- src/config_parser.py
+++ src/config_parser.py
@@ -116,16 +116,16 @@ class MainConfig:
             if self.isPointingToCsvFile(lang, script_path, function):
                 with open(path, 'r') as file:
                     lines = csv.reader(file, delimiter=';')
-                    has_syntax_option = "syntax" in list(lines)[0]
+                    # TODO
+                    # has_syntax_option = "syntax" in list(lines)[0]
+                    # if has_syntax_option:
+                    #     pass
+                    # else:
+                    for param in list(lines)[1:]:
+                        x2_n: list[int | float | str] = self._typifyTestCaseToList(param[1].replace("[", "").replace("]", ""))
+                        y2_n: list[int | float | str] = self._typifyTestCaseToList(param[2].replace("[", "").replace("]", ""))
 
-                    if has_syntax_option:
-                        pass
-                    else:
-                        for param in list(lines)[1:]:
-                            x2_n: list[int | float | str] = self._typifyTestCaseToList(param[1].replace("[", "").replace("]", ""))
-                            y2_n: list[int | float | str] = self._typifyTestCaseToList(param[2].replace("[", "").replace("]", ""))
-
-                            results.append((x2_n, y2_n))
+                        results.append((x2_n, y2_n))
 
         return results
 
