0e998af81f90d510d996863a368ef5d73a31c5a0
makl812c <makl812c@tu-dresden.de>
2026-03-18 14:10:55 +0100

    fixed custom module file for tests

--- tests/configs/config5.py
+++ tests/configs/config5.py
@@ -1,34 +1,50 @@
-from src.config_parser import MainConfig
-from src.hook import HookType
-from src.layer import GeneralLayer, FileLayer, FunctionLayer, to_dict
+from pathlib import Path
+import importlib.util
+
+ROOT = Path(__file__).resolve().parents[2]
+
+# print(ROOT)
+
+spec = importlib.util.spec_from_file_location("config_parser", ROOT / "src/config_parser.py")
+config_parser = importlib.util.module_from_spec(spec) # type: ignore
+spec.loader.exec_module(config_parser) # type: ignore
+
+spec = importlib.util.spec_from_file_location("hook", ROOT / "src/hook.py")
+hook = importlib.util.module_from_spec(spec) # type: ignore
+spec.loader.exec_module(hook) # type: ignore
+
+spec = importlib.util.spec_from_file_location("layer", ROOT / "src/layer.py")
+layer = importlib.util.module_from_spec(spec) # type: ignore
+spec.loader.exec_module(layer) # type: ignore
+
 
 def complex_test():
-    mainConfig1 = MainConfig("../tests/configs/config1.yaml")
+    mainConfig1 = config_parser.MainConfig("../tests/configs/config1.yaml")
 
-    general_layer = GeneralLayer(
-        mainConfig1.getHookGeneral(HookType.GENERAL_SETUP),
-        mainConfig1.getHookGeneral(HookType.GENERAL_SHUTDOWN)
+    general_layer = layer.GeneralLayer(
+        mainConfig1.getHookGeneral(hook.HookType.GENERAL_SETUP),
+        mainConfig1.getHookGeneral(hook.HookType.GENERAL_SHUTDOWN)
     )
 
     for language in mainConfig1.getLanguages():
         if language in mainConfig1.getLanguages() and mainConfig1.content[language] != None:
 
-            file_layer = FileLayer(
+            file_layer = layer.FileLayer(
                 general_layer,
                 language,
-                mainConfig1.getHookFile(HookType.FILE_SETUP, language),
-                mainConfig1.getHookFile(HookType.FILE_SHUTDOWN, language)
+                mainConfig1.getHookFile(hook.HookType.FILE_SETUP, language),
+                mainConfig1.getHookFile(hook.HookType.FILE_SHUTDOWN, language)
             )
 
             for script in mainConfig1.getScripts(language):
                 if script in mainConfig1.content[language] and mainConfig1.content[language][script] != None:
 
-                    function_layer = FunctionLayer(
+                    function_layer = layer.FunctionLayer(
                         file_layer,
                         language,
                         script,
-                        mainConfig1.getHookFunction(HookType.FUNCTION_SETUP, language, script),
-                        mainConfig1.getHookFunction(HookType.FUNCTION_SHUTDOWN, language, script)
+                        mainConfig1.getHookFunction(hook.HookType.FUNCTION_SETUP, language, script),
+                        mainConfig1.getHookFunction(hook.HookType.FUNCTION_SHUTDOWN, language, script)
                     )
                     file_layer.merge(function_layer.toData())
             general_layer.merge(file_layer.toData())
@@ -41,7 +57,7 @@ def complex_test():
                 "function_shutdown": {language: {}},
             })
 
-    return to_dict(general_layer.toData()) == {
+    return layer.to_dict(general_layer.toData()) == {
         "general_setup": {
             "attributes": {
                 "path": "a_random_path"
